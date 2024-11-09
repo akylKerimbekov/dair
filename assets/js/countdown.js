79afae5ca563386866d44d627de2879fbed2edda
Akylbek Kerimbekov <akyl.kerimbekov@gmail.com>
2024-11-09 20:53:58 +0100

    Add countdown and image

---
+++ assets/js/countdown.js
@@ -0,0 +1,30 @@
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+  
+  targetDate = new Date("2024-11-13T00:00:00").getTime();
+
+  const countDown = targetDate,
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+
--- index.markdown
+++ index.markdown
@@ -4,3 +4,27 @@
 
 layout: home
 ---
+
+![Описание изображения](assets/images/Dair.jpg)
+
+<div class="container">
+  <h1 id="headline">Countdown to start my treatment</h1>
+  <div id="countdown">
+    <ul>
+      <li><span id="days"></span>days</li>
+      <li><span id="hours"></span>Hours</li>
+      <li><span id="minutes"></span>Minutes</li>
+      <li><span id="seconds"></span>Seconds</li>
+    </ul>
+  </div>
+  <div id="content" class="emoji">
+    <span>🥳</span>
+    <span>🎉</span>
+    <span>🎂</span>
+  </div>
+</div>
+
+
+<!-- Подключение стилей и скриптов -->
+<link rel="stylesheet" href="{{ '/assets/css/countdown.css' | relative_url }}">
+<script src="{{ '/assets/js/countdown.js' | relative_url }}" defer></script>
